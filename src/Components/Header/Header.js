c26bb4ad8d6a4fcc0f10407a63dc02600a06edc7
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-10-22 22:49:23 +0200

    Adding style to the active link

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,25 @@
 import logo from '../Header/logo.svg'
 import './Header.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 
 export function Header() {
   return (
     <header>
         <img src={logo} alt=""/>
         <nav>
-            <Link to="/">Accueil</Link>
-            <Link to="/about">A propos</Link>
+            <NavLink 
+            to="/" end
+            className={({isActive}) => {
+                return isActive ? "active-link" : ""
+            }}
+            >Accueil</NavLink>
+
+            <NavLink 
+            to="/about"
+            className={({isActive}) => {
+                return isActive ? "active-link" : ""
+            }}
+            >A propos</NavLink>
         </nav>
     </header>
     
