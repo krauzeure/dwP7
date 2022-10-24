f32e68ddd89160e1abb5e06294b6fc18fc7e1e4a
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-10-24 20:59:19 +0200

    Adding a variant height for the about page

--- src/Components/Banner/Banner.js
+++ src/Components/Banner/Banner.js
@@ -1,9 +1,10 @@
 import './Banner.css'
 
 export function Banner(props) {
+
   return (
-    <div className="banner-container">
-        <div className='banner-overlay'></div>
+    <div className={props.source === "about" ? "banner-container banner-container-about" : "banner-container"}>
+        <div className={props.source === "about" ? "banner-overlay banner-overlay-about" : "banner-overlay"}></div>
         <img src={props.img} alt={props.alt}/>
         {props.text && <h1 className="banner-header">{props.text}</h1>}
     </div>
--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -6,12 +6,17 @@ import './About.css'
 export function About() {
   return (
     <>
-      <Banner img={AboutBanner} alt={"montagne enneigées"}/>
+      <Banner img={AboutBanner} alt={"montagne enneigées"} source="about"/>
       <div className="about-collapse">
+
         <Collapse heading="Fiabilité" text="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées  par nos équipes." source="about" />
+
         <Collapse heading="Respect" text="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme." source="about" />
+
         <Collapse heading="Service" text="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question." source="about" />
+
         <Collapse heading="Sécurité" text="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes." source="about" />
+        
       </div>
     </>
   )
