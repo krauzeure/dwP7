62777ab4b4a0d056107f3e1c939a208fded87b84
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-10-24 12:44:08 +0200

    Integrating part of the listing page

--- src/Pages/Listing/Listing.js
+++ src/Pages/Listing/Listing.js
@@ -1,5 +1,40 @@
+import { useParams } from 'react-router-dom'
+import { listings } from '../../data/listings'
+import './Listing.css'
+import { Tag } from '../../Components/Tag/Tag';
+
 export function Listing() {
+
+  const params = useParams();
+  const listingId = params.id;
+
+  const currentListing = listings.filter(el => el.id === listingId);
+  console.log(currentListing)
+
   return (
-    <div>Listing</div>
+    <div className="listing-page">
+      <div className='listing-slideshow'>
+        <img src={currentListing[0].pictures[0]} alt={currentListing[0].title}/>
+      </div>
+
+      <div className="listing-details">
+        <div className="listing-info-host">
+
+          <div className="listing-info">
+            <h1>{currentListing[0].title}</h1>
+            <p>{currentListing[0].location}</p>
+          </div>
+
+          <div className="listing-host">
+            <p>{currentListing[0].host.name}</p>
+            <img src={currentListing[0].host.picture} alt={currentListing[0].host.name} />
+          </div>
+        </div>
+      </div>
+
+      <div className="listing-tag-rating">
+        <Tag text={currentListing[0].tags[0]}/>
+      </div>
+    </div>
   )
 }
