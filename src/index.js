d7a7dddc612894c86f8f67d5781f89ed7fb18001
Cyrielle Chasles <cyrielle.chasles@gmail.com>
2022-10-30 20:45:32 +0100

    Adding a component to scroll back to the top of the page on route change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import './index.css';
 import { AppRoutes } from './Routes'
 import { Header } from './Components/Header/Header'
 import { Footer } from './Components/Footer/Footer'
+import { ScrollToTop } from './Components/ScrollToTop/ScrollToTop';
 
 import { BrowserRouter } from 'react-router-dom'
 import React from 'react';
@@ -12,6 +13,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <>
     <BrowserRouter basename="/dwP7">
+      <ScrollToTop />
       <Header />
       <main className="page-container">
         <AppRoutes />
